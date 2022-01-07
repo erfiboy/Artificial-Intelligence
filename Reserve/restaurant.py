3bbf563c5d2c5e3f572628ebd70d11c62a37ecdd
erfiboy <erfannosrati20@gmail.com>
2022-01-07 20:39:31 +0330

    added H and G

--- Reserve/restaurant.py
+++ Reserve/restaurant.py
@@ -4,14 +4,17 @@ import numpy as np
 import graphviz
 import csv
 from copy import deepcopy
+
 class Node:
     
-    def __init__(self, parent, decision, interval, child = []):
+    def __init__(self, parent, decision, interval, gain, entropy ,child = []):
+        self.name_ = ""
+        self.child_ = child
+        self.gain_ = gain
+        self.entropy_ = entropy
         self.parent_ = parent
         self.decision_ = decision
         self.decision_interval_ = interval
-        self.child_ = child
-        self.name_ = ""
 
     def append_child(self, child):
         self.child_.append(child)
@@ -21,7 +24,6 @@ class Node:
 
 """ parsing the numeric data for train dicision tree
 """
-
 df = pd.read_csv('restaurant.csv')
 col_list = list(df.columns)
 
@@ -44,7 +46,6 @@ for element in column:
     Note: data should be numeric
 """
 def calculate_entropy(data):
-    
     # find unique values 
     unique_values = set(data)
     probabilities = []
@@ -93,11 +94,10 @@ def calculate_remainder(data, goal):
 
 """ calculating the gain base on a data clustring 
 """
-
 def calculate_gain(data, goal):
     data_entropy_before_clustering = calculate_entropy(goal)
     remainder, indexes = calculate_remainder(data, goal)
-    return data_entropy_before_clustering - remainder , indexes
+    return data_entropy_before_clustering - remainder , indexes , data_entropy_before_clustering
 
 """ make the data ready for the next clusters or layers of the tree
 """
@@ -113,7 +113,6 @@ def divide_data_with_indexes(data, index):
 
 """ create a disicion tree
 """
-
 def create_decision_tree(data, name_data, parent, col_header, interval):
     goal_index = len(col_header) -1
     
@@ -123,18 +122,19 @@ def create_decision_tree(data, name_data, parent, col_header, interval):
         else:
             decision = "No"
         
-        node = Node(parent, decision, interval , [])
+        node = Node(parent, decision, interval, 0, 0, [])
         parent.append_child(node)
         return 
     
     maximum_gain = 0
+    entropy = 0
     choosen_attribute_index = ""
     clusters_indexes = []
     
     for attribute in col_header:
         if attribute == col_header[goal_index]:
             continue
-        gain ,indexes = calculate_gain(data[col_header.index(attribute)], data[goal_index])
+        gain ,indexes ,entropy = calculate_gain(data[col_header.index(attribute)], data[goal_index])
         if gain > maximum_gain:
             maximum_gain = gain
             clusters_indexes = indexes
@@ -145,7 +145,7 @@ def create_decision_tree(data, name_data, parent, col_header, interval):
     
     name_of_attribute = name_data.pop(choosen_attribute_index)
     
-    node = Node(parent, node_type, interval, [])
+    node = Node(parent, node_type, interval, maximum_gain, entropy, [])
     parent.append_child(node)
     
     for indexes in clusters_indexes:
@@ -157,7 +157,7 @@ def create_decision_tree(data, name_data, parent, col_header, interval):
     return 
 
 
-root = Node(None, "root", "", [])
+root = Node(None, "root", "", 0, 0, [])
 
 create_decision_tree(wait_data, name_of_data, root, col_list, "")
 
@@ -173,15 +173,19 @@ while stack != []:
     for child in stack[0].child_:
         if stack[0].name_ == 'a':
             name = chr(ord(name)+1)
-            dot.node(child.name_, child.decision_)  
+            entropy = "\n" +"H(V) = " + str(round(child.entropy_,2)) 
+            gain =  "\n" + "G(V) = " + str(round(child.gain_,2)) 
+            dot.node(child.name_, child.decision_ + entropy  + gain) 
         else:
             name = chr(ord(name)+1)
             child.set_name(name)
-            dot.node(child.name_, "(" + str(child.decision_interval_) + ")" + "\n" + child.decision_)  
+            entropy = "\n" +"H(V) = " + str(round(child.entropy_,2)) 
+            gain = "\n" + "G(V) = " + str(round(child.gain_,2))
+            dot.node(child.name_, child.decision_ + entropy  + gain)  
             start = stack[0].name_ 
             end = child.name_
             print(start, end)
-            dot.edge((start), (end))
+            dot.edge((start), (end), "(" + str(child.decision_interval_) + ")")
         stack.append(child)
     stack.pop(0)
 
